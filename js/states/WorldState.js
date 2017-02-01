f2a4fc39060e5cb662da033ba16f9209a9a40b46
Bryant Wang and Erik Killops <bryant@>
2017-02-01 08:53:14 -0800

    Separate Enemy and Flyer into separate JS files, with Flyer inheriting from Enemy.

--- js/states/WorldState.js
+++ js/states/WorldState.js
@@ -192,7 +192,7 @@ TowerDefense.WorldState.prototype.create = function () {
     // _this = this;
     this.game.time.events.loop(1000, function(){
       var randomStartY = (Math.floor(Math.random() * 300)) + 200;
-      var newFlyer = new TowerDefense.Enemy(TowerDefense, 48, randomStartY, 'star');
+      var newFlyer = new TowerDefense.Flyer(TowerDefense, 48, randomStartY, 'star');
       newFlyer.randomEndY = (Math.floor(Math.random() * 300)) + 200;
 
       _this.fliers.add(newFlyer);
@@ -297,128 +297,129 @@ TowerDefense.WorldState.prototype.update = function () {
 
 
 
-// ENEMY STUFF from -- http://blog.intracto.com/create-fun-and-interactive-games-with-javascript-using-phaser.io
-
-// this.monsters;
-// function create() {
-//     …
+// // ENEMY STUFF from -- http://blog.intracto.com/create-fun-and-interactive-games-with-javascript-using-phaser.io
 //
-//     // Enemies
-//     this.monsters = game.add.group();
-//     game.time.events.loop(250, function(){
-//         var enemy = new Enemy(game.world.randomX, game.world.randomY, 'enemy');
-//         enemyGroup.add(enemy);
-//     });
-// }
-
-// function update() {
-//     …
+// // this.monsters;
+// // function create() {
+// //     …
+// //
+// //     // Enemies
+// //     this.monsters = game.add.group();
+// //     game.time.events.loop(250, function(){
+// //         var enemy = new Enemy(game.world.randomX, game.world.randomY, 'enemy');
+// //         enemyGroup.add(enemy);
+// //     });
+// // }
 //
-//     // Collision detection
-//     game.physics.arcade.overlap(character, enemyGroup, collisionHandler, null, this);
-// }
-// this.sprite = game.add.sprite(50, 50, 'car'); // standard car sprite
-TowerDefense.Enemy = function (parentState, posX, posY, sprite) {
-    Phaser.Sprite.call(this, game, posX, posY, sprite);
-    this.outOfBoundsKill = true;
-    this.collisionEnabled = false;
-    this.game.physics.arcade.enable(this, true);
-    this.anchor.setTo(0.5, 0.5);
-    this.body.setSize(16, 16);
-
-    this.type = sprite;
-    this.path = [];
-    this.pathStep = -1;
-    this.counter = 0;
-    this.lastRotation = 0;
-}
-
-TowerDefense.Enemy.prototype = Object.create(Phaser.Sprite.prototype);
-TowerDefense.Enemy.prototype.constructor = TowerDefense.Enemy;
-
-// TowerDefense.Enemy.prototype.create = function () {
+// // function update() {
+// //     …
+// //
+// //     // Collision detection
+// //     game.physics.arcade.overlap(character, enemyGroup, collisionHandler, null, this);
+// // }
+// // this.sprite = game.add.sprite(50, 50, 'car'); // standard car sprite
+// TowerDefense.Enemy = function (parentState, posX, posY, sprite) {
+//     Phaser.Sprite.call(this, game, posX, posY, sprite);
+//     this.outOfBoundsKill = true;
+//     this.collisionEnabled = false;
+//     this.game.physics.arcade.enable(this, true);
+//     this.anchor.setTo(0.5, 0.5);
+//     this.body.setSize(16, 16);
 //
+//     this.type = sprite;
+//     this.path = [];
+//     this.pathStep = -1;
+//     this.counter = 0;
+//     this.lastRotation = 0;
 // }
-
-TowerDefense.Enemy.prototype.update = function () {
-  if(this.type === "star") {
-    this.game.physics.arcade.moveToXY(this, 752, this.randomEndY, 100);
-  } else {
-    //car variables
-    var next_position;
-    this.counter++;
-
-    this.game.physics.arcade.collide(this, TowerDefense.layer2);
-
-    //car movement trigger
-    if (this.path.length > 0) {
-      if(this.pathStep < 0)
-      this.pathStep = 1;
-      next_position = this.path[this.pathStep];
-
-      if (!this.reachedXY(next_position)) {
-        var moveX = (next_position.x * 32) + 16;
-        var moveY = (next_position.y * 32) + 16;
-        this.game.physics.arcade.moveToXY(this, moveX, moveY, 100);
-      } else {
-        this.body.velocity.x = 0;
-        this.body.velocity.y = 0;
-        this.x = (next_position.x * 32) + 16;
-        this.y = (next_position.y * 32) + 16;
-
-        if (this.pathStep < this.path.length - 1) {
-          this.pathStep += 1;
-        } else {
-          this.path = [];
-          this.pathStep = -1;
-        }
-      }
-    } else {
-      this.body.velocity.x = 0;
-      this.body.velocity.y = 0;
-    }
-
-  }
-
-  if(this.counter % 3 === 0) {
-    if(this.body.velocity.x === 0 && this.body.velocity.y === 0) {
-      this.rotation = this.lastRotation;
-    } else if(this.body.velocity.x > 20) {
-      this.rotation = 0;
-      this.lastRotation = 0;
-    } else if(this.body.velocity.y > 20) {
-      this.rotation = 1.57;
-      this.lastRotation = 1.57;
-    } else if(this.body.velocity.x < -20) {
-      this.rotation = 3.14;
-      this.lastRotation = 3.14;
-    } else if(this.body.velocity.y < -20) {
-      this.rotation = 4.71;
-      this.lastRotation = 4.71;
-    } else {
-      this.rotation = 0;
-    }
-  }
-}
-
-TowerDefense.Enemy.prototype.moveAlongXY = function() {
-    "use strict";
-    if (this.path !== null) {
-        this.pathStep = 1;
-    } else {
-        this.path = [];
-    }
-};
-
-TowerDefense.Enemy.prototype.reachedXY = function(position){
-    "use strict";
-    if (this.game.physics.arcade.distanceToXY(this, (position.x * 32)+16, (position.y * 32)+16) <= 3) {
-        return true;
-    } else {
-        return false;
-    }
-};
-
-TowerDefense.Enemy.prototype.setPath = function(path) {
-  this.path = path;
-}
+//
+// TowerDefense.Enemy.prototype = Object.create(Phaser.Sprite.prototype);
+// TowerDefense.Enemy.prototype.constructor = TowerDefense.Enemy;
+//
+// // TowerDefense.Enemy.prototype.create = function () {
+// //
+// // }
+//
+// TowerDefense.Enemy.prototype.update = function () {
+//   if(this.type === "star") {
+//     this.game.physics.arcade.moveToXY(this, 752, this.randomEndY, 100);
+//   } else {
+//     //car variables
+//     var next_position;
+//     this.counter++;
+//
+//     this.game.physics.arcade.collide(this, TowerDefense.layer2);
+//
+//     //car movement trigger
+//     if (this.path.length > 0) {
+//       if(this.pathStep < 0) {
+//         this.pathStep = 0;
+//       }
+//       next_position = this.path[this.pathStep];
+//
+//       if (!this.reachedXY(next_position)) {
+//         var moveX = (next_position.x * 32) + 16;
+//         var moveY = (next_position.y * 32) + 16;
+//         this.game.physics.arcade.moveToXY(this, moveX, moveY, 100);
+//       } else {
+//         this.body.velocity.x = 0;
+//         this.body.velocity.y = 0;
+//         this.x = (next_position.x * 32) + 16;
+//         this.y = (next_position.y * 32) + 16;
+//
+//         if (this.pathStep < this.path.length - 1) {
+//           this.pathStep += 1;
+//         } else {
+//           this.path = [];
+//           this.pathStep = -1;
+//         }
+//       }
+//     } else {
+//       this.body.velocity.x = 0;
+//       this.body.velocity.y = 0;
+//     }
+//
+//   }
+//
+//   if(this.counter % 3 === 0) {
+//     if(this.body.velocity.x === 0 && this.body.velocity.y === 0) {
+//       this.rotation = this.lastRotation;
+//     } else if(this.body.velocity.x > 20) {
+//       this.rotation = 0;
+//       this.lastRotation = 0;
+//     } else if(this.body.velocity.y > 20) {
+//       this.rotation = 1.57;
+//       this.lastRotation = 1.57;
+//     } else if(this.body.velocity.x < -20) {
+//       this.rotation = 3.14;
+//       this.lastRotation = 3.14;
+//     } else if(this.body.velocity.y < -20) {
+//       this.rotation = 4.71;
+//       this.lastRotation = 4.71;
+//     } else {
+//       this.rotation = 0;
+//     }
+//   }
+// }
+//
+// TowerDefense.Enemy.prototype.moveAlongXY = function() {
+//     "use strict";
+//     if (this.path !== null) {
+//         this.pathStep = 1;
+//     } else {
+//         this.path = [];
+//     }
+// };
+//
+// TowerDefense.Enemy.prototype.reachedXY = function(position){
+//     "use strict";
+//     if (this.game.physics.arcade.distanceToXY(this, (position.x * 32)+16, (position.y * 32)+16) <= 3) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// };
+//
+// TowerDefense.Enemy.prototype.setPath = function(path) {
+//   this.path = path;
+// }
