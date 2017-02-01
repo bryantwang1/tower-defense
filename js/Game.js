1f2d189422cefec05bb378eee7612d28d583fedc
Annie Sonna and Joshua Rinard <joshua@>
2017-02-01 14:45:46 -0800

    Created rocket tower

--- js/Game.js
+++ js/Game.js
@@ -1,5 +1,5 @@
 
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'gameContainer', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(1334, 750, Phaser.CANVAS, 'gameContainer', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
 
