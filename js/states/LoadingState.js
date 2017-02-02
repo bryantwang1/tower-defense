bb5be17732930fa4e4700a59c74090e4804caafb
Bryant Wang and Erik Killops <erik@>
2017-02-02 08:43:52 -0800

    import new pngs

--- js/states/LoadingState.js
+++ js/states/LoadingState.js
@@ -28,13 +28,20 @@ TowerDefense.LoadingState.prototype.preload = function () {
     this.game.load.image('rocket-tower', '../assets/rocket-tower48.png');
     this.game.load.image('freeze-tower', '../assets/freeze-tower48.png');
     this.game.load.image('tesla-tower', '../assets/tesla-tower48.png');
-    this.game.load.image('runnerBasic', '../assets/runnerBasic.png');
     this.game.load.image('arrow', '../assets/cannon.png');
-    this.game.load.image('runnerTank', '../assets/runnerTank.png');
-    this.game.load.spritesheet('runner', '../assets/runner2.png', 57.6, 48);
+    // this.game.load.spritesheet('runner', '../assets/runner2.png', 57.6, 48);
     this.game.load.spritesheet('explosion', '../assets/ex1.png', 50, 50);
     this.game.load.spritesheet('explosion-freeze', '../assets/freeze-explosion.png', 50, 50);
-    this.game.load.image("background", "/assets/map.jpg");
+    // new images
+    this.game.load.spritesheet('runnerBasic', '../assets/runnerBasic.png', 48, 48);
+    this.game.load.spritesheet('runnerBasic_2', '../assets/runnerBasic_2.png', 48, 48);
+    this.game.load.spritesheet('runnerTank', '../assets/runnerTank.png', 48, 48);
+    this.game.load.spritesheet('runnerAir', '../assets/runnerAir.png', 48, 48);
+
+    this.game.load.image("gold", "/assets/gold$.png");
+    this.game.load.image("red", "/assets/red.png");
+    this.game.load.image("life", "/assets/life.png");
+    this.game.load.image("lightning", "/assets/lightning.png");
 };
 
 TowerDefense.LoadingState.prototype.create = function () {
