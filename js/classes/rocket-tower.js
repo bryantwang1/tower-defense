6e48a2f5bd1da23209e8ca2eb7a507aa64cb8488
Annie Sonna and Joshua Rinard <annie+joshua@>
2017-02-01 16:58:38 -0800

    fix the hard coding on rocket, freeze and tesla tower. explosion on freeze not yet working

--- js/classes/rocket-tower.js
+++ js/classes/rocket-tower.js
@@ -3,11 +3,11 @@ var TowerDefense = TowerDefense || {};
 
 TowerDefense.RocketTower = function (parentState, posX, posY) {
       // parentState, posX, posY, range, fireRate, dmg, bulletspeed, image
-      TowerDefense.Tower.call(parentState, posX, posY, 'rocket-tower', 600, 800, 5, 200, 'rocket');
+      TowerDefense.Tower.call(this, parentState, posX, posY, 'rocket-tower', 600, 800, 5, 200, 'rocket');
 
 }
 
-TowerDefense.RocketTower.prototype = Object.create(TowerDefense.Enemy.prototype);
+TowerDefense.RocketTower.prototype = Object.create(TowerDefense.Tower.prototype);
 TowerDefense.RocketTower.prototype.constructor = TowerDefense.RocketTower;
 
 TowerDefense.RocketTower.prototype.update = function () {
--- js/classes/tesla-tower.js
+++ js/classes/tesla-tower.js
@@ -3,11 +3,11 @@ var TowerDefense = TowerDefense || {};
 
 TowerDefense.TeslaTower = function (parentState, posX, posY) {
       // parentState, posX, posY, range, fireRate, dmg, bulletspeed, image
-      TowerDefense.Tower.call(parentState, posX, posY, 'tesla-tower', 110, 800, 10, 200, 'diamond');
+      TowerDefense.Tower.call(this, parentState, posX, posY, 'tesla-tower', 110, 800, 10, 200, 'diamond');
 
 }
 
-TowerDefense.TeslaTower.prototype = Object.create(TowerDefense.Enemy.prototype);
+TowerDefense.TeslaTower.prototype = Object.create(TowerDefense.Tower.prototype);
 TowerDefense.TeslaTower.prototype.constructor = TowerDefense.TeslaTower;
 
 TowerDefense.TeslaTower.prototype.update = function () {
